74b4d303b3b891579c671c79e92904faf98145ad
JUNKAI-ZHANG <zjk434681299@163.com>
2023-03-31 16:53:49 +0800

    添加了Proto协议格式

---
+++ src/Tool/Message.cpp
@@ -0,0 +1,119 @@
+#include "../Header/Message.h"
+
+MessageHead::MessageHead(const uint8_t *buffer, int len)
+{
+    if (len < HEAD_SIZE)
+    {
+        m_packageType = BODYTYPE::ErrorPackage;
+        return;
+    }
+
+    // 0~3位存大小
+    m_packageSize = ntohl(*((uint32_t *)buffer));
+    // 4~7位存类型
+    m_packageType = static_cast<BODYTYPE>(ntohl(*((uint32_t *)(buffer + 4))));
+    // 8~11位存token
+    m_token = ntohl(*((uint32_t *)(buffer + 8)));
+    return;
+}
+
+bool MessageHead::SerailizeToArray(uint8_t *buffer, int len) const
+{
+    if (len < HEAD_SIZE)
+    {
+        return false;
+    }
+
+    // 转大端序
+    uint32_t size = htonl(m_packageSize);
+    uint32_t type = htonl(static_cast<uint32_t>(m_packageType));
+    uint32_t token = htonl(static_cast<uint32_t>(m_token));
+
+    uint8_t *tmp = (uint8_t *)(&size);
+    for (int i = 0; i < 4; i++)
+    {
+        buffer[i] = tmp[i];
+    }
+
+    tmp = (uint8_t *)(&type);
+    for (int i = 0; i < 4; i++)
+    {
+        buffer[i + 4] = tmp[i];
+    }
+
+    // 最后4Byte不处理，前端不需要解析token.(算了吧，都写了这么多了，也不差这一点了，hh)
+    tmp = (uint8_t *)(&token);
+    for (int i = 0; i < 4; i++)
+    {
+        buffer[i + 8] = tmp[i];
+    }
+    return true;
+}
+
+// ******************** Body **********************
+
+int MessageBody::length() const
+{
+    if (message == nullptr)
+    {
+        return 0;
+    }
+    return (int)message->ByteSizeLong();
+}
+
+bool MessageBody::ParseFromArray(const uint8_t *data, int len) const
+{
+    if (message == nullptr)
+    {
+        return true;
+    }
+    return message->ParseFromArray(data, len);
+}
+
+bool MessageBody::SerializeToArray(uint8_t *buffer, int len) const
+{
+    if (message == nullptr)
+    {
+        return true;
+    }
+    return message->SerializeToArray(buffer, len);
+}
+
+MessageBody::~MessageBody()
+{
+    delete message;
+}
+
+// ******************** Message **********************
+
+int Message::length() const
+{
+    if (body == nullptr)
+    {
+        return HEAD_SIZE;
+    }
+    return HEAD_SIZE + body->length();
+}
+
+bool Message::SerializeToArray(uint8_t *buffer, int len) const
+{
+    if (len < length())
+    {
+        return false;
+    }
+    if (!head->SerailizeToArray(buffer, len))
+    {
+        return false;
+    }
+    if (body == nullptr)
+    {
+        return true; // 允许发送没有消息体的信息
+    }
+    return body->SerializeToArray(buffer + HEAD_SIZE, len - HEAD_SIZE);
+}
+
+Message::~Message()
+{
+    delete head;
+    delete body;
+}
