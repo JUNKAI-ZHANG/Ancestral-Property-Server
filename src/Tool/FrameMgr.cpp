db2e420a6aa9e8040c0aa5f88b860cbad39c6bf4
JUNKAI-ZHANG <zjk434681299@163.com>
2023-04-12 23:43:41 +0800

    添加了帧的管理类，待使用

---
+++ src/Tool/FrameMgr.cpp
@@ -0,0 +1,61 @@
+#ifndef _FRAME_MGR
+#define _FRAME_MGR
+
+#include "../Header/FrameMgr.h"
+
+FrameMgr::FrameMgr()
+{
+    pool = new ObjectPool();
+}
+
+FrameMgr::~FrameMgr()
+{
+    for (auto &it : frame)
+    {
+        for (Message *it : it.second)
+        {
+            pool->Return(it);
+        }
+        it.second.clear();
+    }
+    frame.clear();
+}
+
+bool FrameMgr::AddFrame(const int& frame_count, Message *message)
+{
+    frame[frame_count].push_back(message);
+    // return DelFrame(frame_count - ONE_HOUR);
+    return true;
+}
+
+bool FrameMgr::DelFrame(const int& frame_count)
+{
+    if (frame.count(frame_count))
+    {
+        for (Message *it : frame[frame_count]) 
+        {
+            pool->Return(it);
+        }
+        frame.erase(frame_count);
+    }
+    return true;
+}
+
+std::vector<Message *> FrameMgr::GetFrame(const int& frame_count)
+{
+    if (frame.count(frame_count))
+    {
+        return frame[frame_count];
+    }
+    else 
+    {
+        return std::vector<Message *>();
+    }
+}
+
+int FrameMgr::GetFrameCount()
+{
+    return tot_count;
+}
+
+#endif
